fe8f35f1cbc931b734ccc9ae9a2666090744738c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-09-24 12:03:08 +0530

    added route error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 // creating browser router v6.4 and greater
 const router = createBrowserRouter([
   {
     // Layout route (doesn't have a path)
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -20,6 +22,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader, // provide loader function to menu route
+        errorElement: <Error />,
       },
       {
         path: "/cart",
